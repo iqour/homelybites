7a0c1aabddd5c6c7999f74a1d3adb7fce3627616
haeri <156439185+stellaS-xy@users.noreply.github.com>
2025-03-14 11:57:33 +0800

    vue and firebase setup

---
+++ HomelyBites/src/firebase.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAuK33SEPwThs4Crhe-a4nKxvy1C7DwY-c",
+    authDomain: "homely-bites-ea0f4.firebaseapp.com",
+    projectId: "homely-bites-ea0f4",
+    storageBucket: "homely-bites-ea0f4.firebasestorage.app",
+    messagingSenderId: "986253910172",
+    appId: "1:986253910172:web:9f1cedcbc0c874f2334d59",
+    measurementId: "G-LX4JMZ81QK"
+  };
+
+  const firebaseApp = initializeApp(firebseConfig);
+
+  export default firebaseApp;
