052d6d136de6880fd52029701ccb3c8f5877f2b3
prishachalak <chalakprisha@gmail.com>
2025-03-16 17:00:30 +0800

    setting up router

---
+++ HomelyBites/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Home Page</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+      return{
+        refreshComp: 0
+      }
+    },
+    methods:{
+      change(){
+        this.refreshComp += 1
+      }
+    }
+}
+</script>
